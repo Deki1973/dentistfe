7e87e7a65b55eb42ebac3eae08022fcff16ca272
Dejan <konidek37@gmail.com>
2025-05-13 20:20:46 +0200

    Improve style

--- src/pages/Client.jsx
+++ src/pages/Client.jsx
@@ -9,6 +9,8 @@ import { Navigate, useNavigate } from "react-router-dom"
 import axios from "axios"
 import { urlHeroku } from "../script/urls"
 
+import "../styles/clientStyle.scss"
+
 const Client = () => {
     
 
@@ -24,7 +26,7 @@ const Client = () => {
 
     const handleGetAll=async()=>{
     
-        window.alert("handle click...")   
+        //window.alert("handle click...")   
         console.log(jwt2) 
         
   //      const jwt1="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmb28xIiwicm9sZXMiOlsiQURNSU4iXSwiZXhwIjoxNzQyNDk1OTc3LCJpYXQiOjE3NDI0OTIzNzd9.QVxNzsrx0QcR0D-L8BG0dOHz5iEurKtidHDkm19hxBU"
@@ -60,13 +62,15 @@ const Client = () => {
 
     const handleSubmit=async (e)=>{
         e.preventDefault()
-        console.log("handle submit...")
+        //console.log("handle submit...")
         console.log(e.nativeEvent.submitter.id)
+
+       
         const clientAttribute=document.getElementById("select1").value
         const clientAttributeValue=document.getElementById("clientAttributeValue").value
 
-        console.log(clientAttribute)
-        console.log(clientAttributeValue)
+        //console.log(clientAttribute)
+        //console.log(clientAttributeValue)
 
         const url=`http://localhost:8080/client/${clientAttribute}/${clientAttributeValue}`
         
@@ -122,22 +126,26 @@ const Client = () => {
     }
     return ( 
         <>
-        <h1>Client page</h1>
+        <h1>Clients</h1>
         
         
-        <button id="btnAddNewClient" onClick={handleAddNewClient}>Add New Client</button>
         
+        <button id="btnAddNewClient" onClick={handleAddNewClient}>Add New</button>
+        <button onClick={handleGetAll} id="btnGetAll">Get All</button>
         <br />
         <form action="submit" onSubmit={handleSubmit}>
-        <select id="select1">
+            
+        <select id="select1" className="select1">
             <option value="id">Clent ID: </option>
             <option value="name">Full Name: </option>
             <option value="contact">Contact: </option>
         </select>
-        <input type="text" id="clientAttributeValue" placeholder="Enter search parameter value: " required="true"/>
+        <input type="text" id="clientAttributeValue" className="clientAttributeValue" placeholder="Enter search parameter value: " required="true"/>
         <button type="submit" id="btnGetClient">Get Client</button>
+        
+        
         </form>
-        <button onClick={handleGetAll}>Get All</button>
+       
         
         
         <div>
--- src/pages/UpdateClient.jsx
+++ src/pages/UpdateClient.jsx
@@ -1,41 +1,42 @@
 import { useEffect } from "react";
-import { useState} from "react";
+import { useState } from "react";
 import { Navigate, useParams } from "react-router-dom";
 import { useJwt } from "../contexts/JwtContext";
 import { useNavigate } from "react-router-dom";
+import "../styles/UpdateClient.scss"
 
 const UpdateClient = () => {
 
-    
 
-    let {id}=useParams()
 
-    const{jwt2}=useJwt()
+    let { id } = useParams()
+
+    const { jwt2 } = useJwt()
 
     //const [clientId, setClientId]=useState(null)
-    const [fullName,setFullName]=useState("")
-    const [contact,setContact]=useState("")
-    const [note,setNote]=useState("")
+    const [fullName, setFullName] = useState("")
+    const [contact, setContact] = useState("")
+    const [note, setNote] = useState("")
 
-    const navigate=useNavigate()
+    const navigate = useNavigate()
 
-    const getClientById=async(e)=>{
-        console.log("get client by id..."+id)
-        const response=await fetch(`http://localhost:8080/client/id/${id}`,
+    const getClientById = async (e) => {
+        console.log("get client by id..." + id)
+        const response = await fetch(`http://localhost:8080/client/id/${id}`,
             {
-                method:"GET",
-                mode:"cors",
-                headers:{
+                method: "GET",
+                mode: "cors",
+                headers: {
                     'Authorization': `Bearer ${jwt2}`,
                     'Content-Type': 'application/json',
                 }
             }
         )
         console.log(response)
-        const clientJson=await response.json()
+        const clientJson = await response.json()
         console.log("client json...")
         console.log(clientJson)
-        
+
         console.log(clientJson.fullName)
         console.log(clientJson.contact)
         console.log(clientJson.note)
@@ -44,106 +45,124 @@ const UpdateClient = () => {
         setContact(clientJson.contact)
         setNote(clientJson.note)
         return "aaaaa"
-        
+
 
     }
 
-    
-    const handleSubmit=async(e)=>{
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
         console.log(e)
         console.log("submitter...")
         console.log(e.nativeEvent.submitter.id)
 
-        if(e.nativeEvent.submitter.id==="btnSave"){
-        const response=await fetch(`http://localhost:8080/client/${id}`, {
-            method:"PUT",
-            mode:"cors",
-            body:JSON.stringify({
-             fullName:fullName,
-             contact:contact,
-             note:note
-               
-            }),
-            headers:{
-             'Authorization': `Bearer ${jwt2}`,
-             'Content-Type': 'application/json',
-         }
-         });
-         console.log(response)
-         const clientJson=await response.json()
-         console.log(clientJson)
-        console.log(e)
-    }
+        if (e.nativeEvent.submitter.id === "btnSave") {
+            const response = await fetch(`http://localhost:8080/client/${id}`, {
+                method: "PUT",
+                mode: "cors",
+                body: JSON.stringify({
+                    fullName: fullName,
+                    contact: contact,
+                    note: note
 
-
-    if(e.nativeEvent.submitter.id==="btnBack"){
-        navigate("/client")
-    }
-
-    if(e.nativeEvent.submitter.id==="btnDelete"){
-        window.alert("delete")
-
-        console.log(id)
-
-        const response=await fetch(`http://localhost:8080/client/${id}`,
-            {
-                method:"DELETE",
-                mode:"cors",
-                headers:{
-                 'Authorization': `Bearer ${jwt2}`,
-                'Content-type': 'application/json'
+                }),
+                headers: {
+                    'Authorization': `Bearer ${jwt2}`,
+                    'Content-Type': 'application/json',
                 }
-            }
-        )
-        console.log(response)
-        const data=await response.json()
-        console.log(data)
+            });
+            console.log(response)
+            const clientJson = await response.json()
+            console.log(clientJson)
+            console.log(e)
+        }
 
 
-        
+        if (e.nativeEvent.submitter.id === "btnBack") {
+            navigate("/client")
+        }
+
+        if (e.nativeEvent.submitter.id === "btnDelete") {
+            window.alert("delete")
+
+            console.log(id)
+
+            const response = await fetch(`http://localhost:8080/client/${id}`,
+                {
+                    method: "DELETE",
+                    mode: "cors",
+                    headers: {
+                        'Authorization': `Bearer ${jwt2}`,
+                        'Content-type': 'application/json'
+                    }
+                }
+            )
+            console.log(response)
+            const data = await response.json()
+            console.log(data)
+
+
+
+        }
     }
-}
-   
+
 
 
     useEffect(() => {
         //const singleBook = books.filter((item) => item?._id == id);
         //console.log(singleBook);
         console.log("use effect...")
-       
-        const odgovor=getClientById()
-        console.log(odgovor)
-        
-        
-        console.log("Client data: "+fullName+"|"+contact+"|"+note)
-        
-      }, []);
-    
 
-    
-    return ( 
-        <>
-        <h1>client update</h1>
-        <form action="submit" onSubmit={handleSubmit}>
-            <input type="text" placeholder="Full Name: " value={fullName} onChange={e=>{
-                setFullName(e.target.value)
-            }}/>
-            <input type="text" placeholder="Contact: " value={contact} onChange={e=>{
-                setContact(e.target.value)
-            }}/>
-            <textarea placeholder="Note: " value={note} onChange={e=>{
-                setNote(e.target.value)
-            }}></textarea>
-            <button type="submit" id="btnSave">Save</button>
-            <button type="submit" id="btnBack">Back</button>
-            <button type="submit" id="btnDelete">Delete</button>
-            
-            
-        </form>
-        </>
-     );
+        const odgovor = getClientById()
+        console.log(odgovor)
+
+
+        console.log("Client data: " + fullName + "|" + contact + "|" + note)
+
+    }, []);
+
+
+
+    return (
+        <div className="updateClient">
+            <h2>Update {fullName},ID:{id} data:</h2>
+            <form action="submit" onSubmit={handleSubmit}>
+
+                <table>
+                    <tbody>
+                        <tr><td>ID: </td><td>{id}</td></tr>
+                        <tr>
+                            <td> <label>Full Name: </label></td><td> <input type="text" placeholder="Full Name: " value={fullName} onChange={e => {
+                                setFullName(e.target.value)
+                            }} /></td>
+                        </tr>
+                        <tr>
+                            <td><label>Contact: </label></td><td> <input type="text" placeholder="Contact: " value={contact} onChange={e => {
+                                setContact(e.target.value)
+                            }} /></td>
+                        </tr>
+                        <tr>
+                            <td>  <label>Note: </label></td><td><textarea placeholder="Note: " value={note} onChange={e => {
+                                setNote(e.target.value)
+                            }}></textarea></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
+
+                <br />
+                <button type="submit" id="btnSave">Save</button>
+                <button type="submit" id="btnBack">Back</button>
+                <button type="submit" id="btnDelete">Delete</button>
+
+
+
+
+            </form>
+        </div>
+    );
 
 }
- 
+
 export default UpdateClient;
