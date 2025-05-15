75dded51d97f514d3cdcb743b3b29bf68a6296d5
Dejan <konidek37@gmail.com>
2025-05-15 17:44:40 +0200

    Improve style

--- src/pages/AddNewClient.jsx
+++ src/pages/AddNewClient.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useJwt } from "../contexts/JwtContext";
 import { useNavigate } from "react-router-dom";
+import "../styles/AddNewClient.scss"
 
 
 const AddNewClient = () => {
