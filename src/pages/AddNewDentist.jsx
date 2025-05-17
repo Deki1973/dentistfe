86206614aa7ea535f918cfa012944472b12efe50
Dejan <konidek37@gmail.com>
2025-05-17 20:25:27 +0200

    Improve UI

--- src/pages/AddNewDentist.jsx
+++ src/pages/AddNewDentist.jsx
@@ -2,69 +2,86 @@ import { useState } from "react";
 import { useJwt } from "../contexts/JwtContext";
 import { useNavigate } from "react-router-dom";
 
+import "../styles/AddNewDentist.scss"
+
 const AddNewDentist = () => {
 
-  let {jwt2}=useJwt();
-  const[fullName,setFullName]=useState("")
-  const[contact,setContact]=useState("")
- 
-  const navigate=useNavigate()
+  let { jwt2 } = useJwt();
+  const [fullName, setFullName] = useState("")
+  const [contact, setContact] = useState("")
 
-const handleSubmit=async (e)=>{
-  e.preventDefault()
+  const navigate = useNavigate()
 
-  if(e.nativeEvent.submitter.id==="btnCancel"){
-    console.log("handle cancel")
-    navigate("/dentist")
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault()
 
-if (e.nativeEvent.submitter.id==="btnSave"){
-  console.log("handle save")
-}
+    if (e.nativeEvent.submitter.id === "btnCancel" || e.nativeEvent.submitter.id === "btnBack") {
+      console.log("handle cancel ")
+      navigate("/dentist")
+    }
 
-const response=await fetch(`http://localhost:8080/dentist`,{
-  method:"POST",
-  mode:"cors",
-  body:JSON.stringify({
-      fullName:fullName,
-      contact:contact,
+    if (e.nativeEvent.submitter.id === "btnSave") {
+      console.log("handle save")
+      const response = await fetch(`http://localhost:8080/dentist`, {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({
+          fullName: fullName,
+          contact: contact,
 
-  }),
-  
-      headers:{
+        }),
+
+        headers: {
           'Authorization': `Bearer ${jwt2}`,
           'Content-Type': 'application/json',
-      }
-})
-console.log(response)
-const data=await response.json();
-console.log("Successfuly added: ")
-console.log(data)
-window.alert(`A new dentist successfully added: \n${data.dentistId}\n${data.fullName}\n${data.contact}`)
+        }
+      })
+      console.log(response)
+      const data = await response.json();
+      console.log("Successfuly added: ")
+      console.log(data)
+      window.alert(`A new dentist successfully added: \n${data.dentistId}\n${data.fullName}\n${data.contact}`)
+
+    }
+
+
+  }
+
+  return (
+    <div className="addNewDentist">
+
+      <h1>Add new dentist</h1>
+      <form action="submit" onSubmit={handleSubmit}>
+
+        <table>
+          <tbody>
+            <tr>
+              <td>Full Name: </td><td><input type="text" id="inputFullName"
+                placeholder="Full Name: " value={fullName}
+                onChange={e => {
+                  setFullName(e.target.value)
+                }} /></td>
+            </tr>
+            <tr>
+              <td>Contact: </td><td>  <input type="text" id="inputContact"
+                placeholder="Contact: "
+                value={contact}
+                onChange={e => {
+                  setContact(e.target.value)
+                }} /></td>
+            </tr>
+          </tbody>
+        </table>
+
+
+
+        <button type="submit" id="btnSave">Save</button>
+        <button type="submit" id="btnBack">Back</button>
+        <button type="submit" id="btnCancel">Cancel</button>
+
+      </form>
+    </div>
+  );
 }
 
-    return (
-        <>
-        
-        <h1>Add new dentist</h1>
-        <form action="submit" onSubmit={handleSubmit}>
-
-        <input type="text" id="inputFullName" 
-            placeholder="Full Name: " value={fullName} 
-            onChange={e=>{
-                setFullName(e.target.value)}}/>
-            <input type="text" id="inputContact" 
-            placeholder="Contact: " 
-            value={contact} 
-            onChange={e=>{
-                setContact(e.target.value)
-            }}/>
-  <button type="submit" id="btnSave">Save</button>
-  <button type="submit" id="btnCancel">Cancel</button>
-
-        </form>
-        </>
-      );
-}
- 
 export default AddNewDentist;
--- src/pages/UpdateDentist.jsx
+++ src/pages/UpdateDentist.jsx
@@ -1,107 +1,111 @@
 import { useEffect } from "react";
-import { useState} from "react";
+import { useState } from "react";
 import { Navigate, useParams } from "react-router-dom";
 import { useJwt } from "../contexts/JwtContext";
 import { useNavigate } from "react-router-dom";
+import "../styles/UpdateDentist.scss"
 
 const UpdateDentist = () => {
 
-    let {id}=useParams()
+    let { id } = useParams()
 
-    const{jwt2}=useJwt()
+    const { jwt2 } = useJwt()
 
-    const [fullName,setFullName]=useState("")
-    const [contact,setContact]=useState("")
-    
+    const [dentistId, setDentistId] = useState(null)
+    const [fullName, setFullName] = useState("")
+    const [contact, setContact] = useState("")
 
-    const navigate=useNavigate()
 
-    const getDentistById=async(e)=>{
-        console.log("get dentist by id..."+id)
-        const response=await fetch(`http://localhost:8080/dentist/id/${id}`,
+
+    const navigate = useNavigate()
+
+    const getDentistById = async (e) => {
+        console.log("get dentist by id..." + id)
+        const response = await fetch(`http://localhost:8080/dentist/id/${id}`,
             {
-                method:"GET",
-                mode:"cors",
-                headers:{
+                method: "GET",
+                mode: "cors",
+                headers: {
                     'Authorization': `Bearer ${jwt2}`,
                     'Content-Type': 'application/json',
                 }
             }
         )
         console.log(response)
-        const data=await response.json()
+        const data = await response.json()
         console.log("dentist json...")
         console.log(data)
-        
+
         console.log(data.fullName)
         console.log(data.contact)
-        
+
 
         setFullName(data.fullName)
         setContact(data.contact)
-        
-        
+        setDentistId(data.dentistId)
+
+
 
     }
 
-    const handleSubmit=async (e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault()
-       console.log("handle submit...")
-       console.log(e.nativeEvent.submitter.id)
+        console.log("handle submit...")
+        console.log(e.nativeEvent.submitter.id)
 
-       if(e.nativeEvent.submitter.id==="btnSave"){
-        const response=await fetch(`http://localhost:8080/dentist/${id}`, {
-            method:"PUT",
-            mode:"cors",
-            body:JSON.stringify({
-             fullName:fullName,
-             contact:contact,
-               
-            }),
-            headers:{
-             'Authorization': `Bearer ${jwt2}`,
-             'Content-Type': 'application/json',
-         }
-         });
-         console.log(response)
-         const data=await response.json()
-         console.log(data)
-        console.log(e)
-    }
+        if (e.nativeEvent.submitter.id === "btnSave") {
+            const response = await fetch(`http://localhost:8080/dentist/${id}`, {
+                method: "PUT",
+                mode: "cors",
+                body: JSON.stringify({
+                    fullName: fullName,
+                    contact: contact,
 
-
-       if(e.nativeEvent.submitter.id==="btnBack"){
-        navigate("/client")
-    }
-
-    
-    if(e.nativeEvent.submitter.id==="btnDelete"){
-        const message=`WARNING! This operation cannot be undone!\nAre you sure?`
-        
-        if (window.confirm(message)!==true){
-            return
+                }),
+                headers: {
+                    'Authorization': `Bearer ${jwt2}`,
+                    'Content-Type': 'application/json',
+                }
+            });
+            console.log(response)
+            const data = await response.json()
+            console.log(data)
+            console.log(e)
         }
 
-        console.log(id)
 
-        const response=await fetch(`http://localhost:8080/dentist/${id}`,
-            {
-                method:"DELETE",
-                mode:"cors",
-                headers:{
-                 'Authorization': `Bearer ${jwt2}`,
-                'Content-type': 'application/json'
-                }
+        if (e.nativeEvent.submitter.id === "btnBack") {
+            navigate("/dentist")
+        }
+
+
+        if (e.nativeEvent.submitter.id === "btnDelete") {
+            const message = `WARNING! This operation cannot be undone!\nAre you sure?`
+
+            if (window.confirm(message) !== true) {
+                return
             }
-        )
-        console.log(response)
-        const data=await response.json()
-        console.log(data)   
-    }
+
+            console.log(id)
+
+            const response = await fetch(`http://localhost:8080/dentist/${id}`,
+                {
+                    method: "DELETE",
+                    mode: "cors",
+                    headers: {
+                        'Authorization': `Bearer ${jwt2}`,
+                        'Content-type': 'application/json'
+                    }
+                }
+            )
+            console.log(response)
+            const data = await response.json()
+            console.log(data)
+        }
 
 
-       
-       
+
+
 
     }
 
@@ -109,36 +113,50 @@ const UpdateDentist = () => {
         //const singleBook = books.filter((item) => item?._id == id);
         //console.log(singleBook);
         console.log("use effect...")
-       
-        const odgovor=getDentistById()
+
+        const odgovor = getDentistById()
         console.log(odgovor)
-        
-        
-        console.log("Dentist data: "+fullName+"|"+contact)
-        
-      }, []);
 
 
-    return ( 
-        <>
-        <h1>Update Dentist</h1>
-        <p>Dentist ID: {id}</p>
-        <p>Full Name: {fullName}</p>
-        <p>Contact: {contact}</p>
-        <form action="submit" onSubmit={handleSubmit}>
+        console.log("Dentist data: " + fullName + "|" + contact)
 
-        <input type="text" placeholder="Full Name: " value={fullName} onChange={e=>{
-                setFullName(e.target.value)
-            }}/>
-            <input type="text" placeholder="Contact: " value={contact} onChange={e=>{
-                setContact(e.target.value)
-            }}/>
-            <button type="submit" id="btnSave">Save</button>
-            <button type="submit" id="btnBack">Back</button>
-            <button type="submit" id="btnDelete">Delete</button>
-        </form>
-        </>
-     );
+    }, []);
+
+
+    return (
+        <div className="updateDentist">
+            <h1>Update Dentist</h1>
+
+            <form action="submit" onSubmit={handleSubmit}>
+
+                <table>
+                    <tbody>
+                        <tr>
+                            <td className="left">Dentist ID: </td><td className="right">{dentistId}</td>
+                        </tr>
+                        <tr>
+                            <td className="left">Full Name: </td>
+                            <td className="right"><input type="text" placeholder="Full Name: " value={fullName} onChange={e => {
+                                setFullName(e.target.value)
+                            }} /></td>
+                        </tr>
+                        <tr>
+                            <td className="left">Contact: </td>
+                            <td className="right">  <input type="text" placeholder="Contact: " value={contact} onChange={e => {
+                                setContact(e.target.value)
+                            }} /></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
+
+                <button type="submit" id="btnSave">Save</button>
+                <button type="submit" id="btnBack">Back</button>
+                <button type="submit" id="btnDelete">Delete</button>
+            </form>
+        </div>
+    );
 }
- 
+
 export default UpdateDentist;
