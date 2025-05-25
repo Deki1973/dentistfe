4f31f8916b552a22e7718bfa6e38f350ca9b9096
Dejan <konidek37@gmail.com>
2025-05-25 15:57:25 +0200

    Change urls

--- src/script/urls.js
+++ src/script/urls.js
@@ -1,2 +1,2 @@
-export const urlHerokuu=`https://stark-caverns-43674-8eb4f8cb65af.herokuapp.com`
+export const urlHeroku=`https://stark-caverns-43674-8eb4f8cb65af.herokuapp.com`
 export const urlLocal=`http://localhost:8080`
